227b0e5ab8bf98fdd00660dca01fcc5c2789d22f
peet <teilzeitjobbewerber@gmx.de>
2024-10-19 18:34:26 +0200

    TODO: todos added

---
+++ TODO.js
@@ -0,0 +1,7 @@
+//----------- 19.10.2024 -----------//
+
+// TODO Ideen sammeln
+// TODO Wiederverwertbaren header gestalten
+// TODO Icons oder Fotos besorgen
+// TODO mit der Übersichts-Seite anfangen
+
