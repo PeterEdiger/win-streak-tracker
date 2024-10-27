f7f937719043c16296712d3b6a87143f0452db11
peet <teilzeitjobbewerber@gmx.de>
2024-10-27 16:07:58 +0100

    Route: exp route added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ExperimentsComponent } from './experiments/experiments.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "exp", component: ExperimentsComponent}
+];
+
+
